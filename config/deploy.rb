048c94e5aae9cc5cee9b2ce6033a676b75eb9117
skji <jishankai@qq.com>
2015-12-17 20:30:36 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,21 +43,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 namespace :deploy do
-  namespace :assets do
-      task :precompile, :roles => :web do
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-          run_locally("rake assets:clean && rake assets:precompile")
-          run_locally "cd public && tar -jcf assets.tar.bz2 assets"
-          top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
-          run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-          run_locally "rm public/assets.tar.bz2"
-          run_locally("rake assets:clean")
-        else
-          logger.info "Skipping asset precompilation because there were no asset changes"
-        end
-      end
-  end
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
